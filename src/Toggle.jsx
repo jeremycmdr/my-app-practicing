dd6664c88f6bd6ce56273ade17a34cb3b3336605
jeremycmdr <115502515+jeremycmdr@users.noreply.github.com>
2024-04-11 21:39:20 +0200

    Object state instead of multiple smaller states

--- src/Toggle.jsx
+++ src/Toggle.jsx
@@ -3,19 +3,92 @@ import "./Toggle.css";
 
 const Toggle = (props) => {
   const [isActive, setIsActive] = useState(false);
+  const [form, setForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    adress: "",
+    zipCode: "",
+  });
 
   const toggleHandler = () => {
     setIsActive(!isActive);
     props.setToggleActive(!isActive);
   };
 
+  const handleFormChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //can use like this too
+  /* const handleFormChange = (e) => {
+    setForm((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  }; */
+
+  //can use like this too
+  /* const handleFormChange = (e) => {
+    setForm((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  }; */
+
   return (
-    <div
-      className={`toggle ${isActive ? "active" : ""}`}
-      onClick={toggleHandler}
-    >
-      <div className="slider"></div>
-    </div>
+    <>
+      <div
+        className={`toggle ${isActive ? "active" : ""}`}
+        onClick={toggleHandler}
+      >
+        <div className="slider"></div>
+      </div>
+      <div>
+        <form>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            className="form_input"
+            onChange={handleFormChange}
+          ></input>
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            className="form_input"
+            onChange={handleFormChange}
+          ></input>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            className="form_input"
+            onChange={handleFormChange}
+          ></input>
+          <input
+            type="text"
+            name="adress"
+            placeholder="Adress"
+            className="form_input"
+            onChange={handleFormChange}
+          ></input>
+          <input
+            type="text"
+            name="zipCode"
+            placeholder="Zip Code"
+            className="form_input"
+            onChange={handleFormChange}
+          ></input>
+        </form>
+      </div>
+    </>
   );
 };
 
